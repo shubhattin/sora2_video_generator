3ae66d885417b0b0638438753413653255382ebe
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-10-19 22:23:06 +0530

    rm token

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,44 +1,12 @@
 import { hc } from 'hono/client';
 import type { Router } from './api_router';
-import { authClient } from '@/lib/auth-client';
-import z from 'zod';
-
-const token_ref: {
-  jwt_token: string | null;
-} = {
-  jwt_token: null
-};
-
-const renewToken = async () => {
-  if (typeof window === 'undefined') return;
-  authClient.token().then((token) => {
-    if (!token.error) {
-      token_ref.jwt_token = token.data.token;
-      const payload = z
-        .object({
-          exp: z.number()
-        })
-        .parse(JSON.parse(atob(token.data.token.split('.')[1])));
-      const exp = new Date(payload.exp * 1000);
-      const now = new Date();
-      const time_diff = exp.getTime() - now.getTime();
-      setTimeout(renewToken, time_diff - 1000); // 1 second before expiration new renew token
-    }
-  });
-};
 
 export const client = hc<Router>(
   (typeof window !== 'undefined' ? window.location.origin : '') + '/api',
   {
-    headers: () => ({
-      Authorization: `Bearer ${token_ref.jwt_token}`
-    })
+    headers: () => ({})
   }
 );
-
-if (typeof window !== 'undefined') {
-  renewToken();
-}
 
 export const QUERY_KEYS = {
   video: {
